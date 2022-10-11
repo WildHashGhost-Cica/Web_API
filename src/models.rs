373c7421a05adc20735921f5f18e0491ea508575
WildHashGhost-Cica <wildhashghost@gmail.com>
2022-10-11 18:14:36 +0100

    models

---
+++ src/models.rs
@@ -0,0 +1,35 @@
+use chrono::datetime::DateTime;
+use chrono::offset::utc::Utc;
+use uuid::Uuid;
+use serde::{Deserialize,Serialize};
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct Post {
+    title: String,
+    body: String,
+    author: String,
+    datetime: DateTime<Utc>,
+    uuid:Uuid,
+}
+
+impl Post {
+    pub fn new(
+    title: &str,
+    body: &str, 
+    author: &str,
+    datetime: DateTime<Utc>,
+    uuid: Uuid,
+    ) -> Post {
+        Post {
+        title: title.to_string(),
+        body: body.to_string(),
+        author: author.to_string(),
+        datetime,
+        uuid,
+        }
+    }
+
+    pub fn uuid(&self) -> Uuid{
+        self.uuid
+    }
+}
